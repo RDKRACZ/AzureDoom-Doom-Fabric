ee3d2bf1f2f443f6b214700f6457ca33aa7d4775
AzureZhen <rcraft@cyberconnects2.com>
2020-08-09 21:07:26 -0400

    forgot left over code

--- src/main/java/mod/azure/doom/DoomMod.java
+++ src/main/java/mod/azure/doom/DoomMod.java
@@ -7,15 +7,11 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 public class DoomMod implements ModInitializer {
 
 	public static final String MODID = "doom";
-
-	public static final Identifier MY_SOUND_ID = new Identifier(MODID + ":my_sound");
-	public static SoundEvent MY_SOUND_EVENT = new SoundEvent(MY_SOUND_ID);
 
 	public static final ItemGroup DoomItemGroup = FabricItemGroupBuilder.create(new Identifier(MODID, "other"))
 			.icon(() -> new ItemStack(DoomItems.ARGENT_AXE)).build();
