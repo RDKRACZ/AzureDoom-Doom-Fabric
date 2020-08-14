8a39228b4772f745539812edbd584a2415b11900
AzureZhen <rcraft@cyberconnects2.com>
2020-08-13 22:34:45 -0400

    New E1M1 texture, new Argent Block and Argent Lamp

---
+++ src/main/java/mod/azure/doom/block/ArgentLampBlock.java
@@ -0,0 +1,11 @@
+package mod.azure.doom.block;
+
+import net.minecraft.block.Block;
+
+public class ArgentLampBlock extends Block {
+
+	public ArgentLampBlock(Settings properties) {
+		super(properties);
+	}
+
+}
--- src/main/java/mod/azure/doom/util/DoomBlocks.java
+++ src/main/java/mod/azure/doom/util/DoomBlocks.java
@@ -1,6 +1,8 @@
 package mod.azure.doom.util;
 
 import mod.azure.doom.DoomMod;
+import mod.azure.doom.block.ArgentBlock;
+import mod.azure.doom.block.ArgentLampBlock;
 import mod.azure.doom.block.BarrelBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -16,9 +18,24 @@ public class DoomBlocks {
 	public static final Block BARREL_BLOCK = new BarrelBlock(
 			FabricBlockSettings.of(Material.METAL).sounds(BlockSoundGroup.METAL).nonOpaque());
 
+	public static final Block ARGENT_BLOCK = new ArgentBlock(
+			FabricBlockSettings.of(Material.LAVA).sounds(BlockSoundGroup.BONE).nonOpaque());
+
+	public static final Block ARGENT_LAMP_BLOCK = new ArgentLampBlock(
+			FabricBlockSettings.of(Material.LAVA).sounds(BlockSoundGroup.BONE).lightLevel(15).nonOpaque());
+
 	public static void init() {
 		Registry.register(Registry.BLOCK, new Identifier(DoomMod.MODID, "barrel"), DoomBlocks.BARREL_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier(DoomMod.MODID, "barrel"),
 				new BlockItem(DoomBlocks.BARREL_BLOCK, new Item.Settings().group(DoomMod.DoomItemGroup)));
+
+		Registry.register(Registry.BLOCK, new Identifier(DoomMod.MODID, "argent_block"), DoomBlocks.ARGENT_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(DoomMod.MODID, "argent_block"),
+				new BlockItem(DoomBlocks.ARGENT_BLOCK, new Item.Settings().group(DoomMod.DoomItemGroup)));
+
+		Registry.register(Registry.BLOCK, new Identifier(DoomMod.MODID, "argent_lamp_block"),
+				DoomBlocks.ARGENT_LAMP_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(DoomMod.MODID, "argent_lamp_block"),
+				new BlockItem(DoomBlocks.ARGENT_LAMP_BLOCK, new Item.Settings().group(DoomMod.DoomItemGroup)));
 	}
 }
