4a7d480b63a439ddf311c9e9a3ff4763d9ba1330
AzureZhen <rcraft@cyberconnects2.com>
2020-09-13 10:32:52 -0400

    Projectile not unloading fix

--- src/main/java/mod/azure/doom/entity/projectiles/ArgentBoltEntity.java
+++ src/main/java/mod/azure/doom/entity/projectiles/ArgentBoltEntity.java
@@ -1,13 +1,10 @@
 package mod.azure.doom.entity.projectiles;
 
-import java.util.Iterator;
-
 import mod.azure.doom.util.ProjectilesEntityRegister;
 import mod.azure.doom.util.packets.EntityPacket;
 import mod.azure.doom.util.registry.DoomItems;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -16,15 +13,11 @@ import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Packet;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
@@ -61,11 +54,18 @@ public class ArgentBoltEntity extends PersistentProjectileEntity {
 		return EntityPacket.createPacket(this);
 	}
 
-	protected void func_225516_i_() {
+	@Override
+	protected void age() {
 		++this.ticksInAir;
 		if (this.ticksInAir >= 40) {
 			this.remove();
 		}
+	}
+
+	@Override
+	public void setVelocity(double x, double y, double z, float speed, float divergence) {
+		super.setVelocity(x, y, z, speed, divergence);
+		this.ticksInAir = 0;
 	}
 
 	@Override
@@ -80,7 +80,6 @@ public class ArgentBoltEntity extends PersistentProjectileEntity {
 		this.ticksInAir = tag.getShort("life");
 	}
 
-	@SuppressWarnings("rawtypes")
 	@Override
 	public void tick() {
 		super.tick();
@@ -93,54 +92,26 @@ public class ArgentBoltEntity extends PersistentProjectileEntity {
 			this.prevYaw = this.yaw;
 			this.prevPitch = this.pitch;
 		}
-
-		BlockPos blockPos = this.getBlockPos();
-		BlockState blockState = this.world.getBlockState(blockPos);
-		Vec3d vec3d4;
-		if (!blockState.isAir() && !bl) {
-			VoxelShape voxelShape = blockState.getCollisionShape(this.world, blockPos);
-			if (!voxelShape.isEmpty()) {
-				vec3d4 = this.getPos();
-				Iterator var7 = voxelShape.getBoundingBoxes().iterator();
-
-				while (var7.hasNext()) {
-					Box box = (Box) var7.next();
-					if (box.offset(blockPos).contains(vec3d4)) {
-						this.inGround = true;
-						break;
-					}
-				}
-			}
+		if (this.age >= 600) {
+			this.remove();
 		}
-
-		if (this.shake > 0) {
-			--this.shake;
-		}
-
-		if (this.isTouchingWaterOrRain()) {
-			this.extinguish();
-		}
-
 		if (this.inAir && !bl) {
-			this.method_26352();
-
+			this.age();
 			++this.timeInAir;
 		} else {
 			this.timeInAir = 0;
 			Vec3d vec3d3 = this.getPos();
-			vec3d4 = vec3d3.add(vec3d);
-			HitResult hitResult = this.world.raycast(new RaycastContext(vec3d3, vec3d4,
+			Vec3d vector3d3 = vec3d3.add(vec3d);
+			HitResult hitResult = this.world.raycast(new RaycastContext(vec3d3, vector3d3,
 					RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, this));
 			if (((HitResult) hitResult).getType() != HitResult.Type.MISS) {
-				vec3d4 = ((HitResult) hitResult).getPos();
+				vector3d3 = ((HitResult) hitResult).getPos();
 			}
-
 			while (!this.removed) {
-				EntityHitResult entityHitResult = this.getEntityCollision(vec3d3, vec3d4);
+				EntityHitResult entityHitResult = this.getEntityCollision(vec3d3, vector3d3);
 				if (entityHitResult != null) {
 					hitResult = entityHitResult;
 				}
-
 				if (hitResult != null && ((HitResult) hitResult).getType() == HitResult.Type.ENTITY) {
 					Entity entity = ((EntityHitResult) hitResult).getEntity();
 					Entity entity2 = this.getOwner();
@@ -150,31 +121,19 @@ public class ArgentBoltEntity extends PersistentProjectileEntity {
 						entityHitResult = null;
 					}
 				}
-
 				if (hitResult != null && !bl) {
 					this.onCollision((HitResult) hitResult);
 					this.velocityDirty = true;
 				}
-
 				if (entityHitResult == null || this.getPierceLevel() <= 0) {
 					break;
 				}
-
 				hitResult = null;
 			}
-
 			vec3d = this.getVelocity();
 			double d = vec3d.x;
 			double e = vec3d.y;
 			double g = vec3d.z;
-			if (this.isCritical()) {
-				for (int i = 0; i < 4; ++i) {
-					this.world.addParticle(ParticleTypes.CRIT, this.getX() + d * (double) i / 4.0D,
-							this.getY() + e * (double) i / 4.0D, this.getZ() + g * (double) i / 4.0D, -d, -e + 0.2D,
-							-g);
-				}
-			}
-
 			double h = this.getX() + d;
 			double j = this.getY() + e;
 			double k = this.getZ() + g;
@@ -184,36 +143,19 @@ public class ArgentBoltEntity extends PersistentProjectileEntity {
 			} else {
 				this.yaw = (float) (MathHelper.atan2(d, g) * 57.2957763671875D);
 			}
-
 			this.pitch = (float) (MathHelper.atan2(e, (double) l) * 57.2957763671875D);
 			this.pitch = updateRotation(this.prevPitch, this.pitch);
 			this.yaw = updateRotation(this.prevYaw, this.yaw);
 			float m = 0.99F;
-			if (this.isTouchingWater()) {
-				for (int o = 0; o < 4; ++o) {
-					this.world.addParticle(ParticleTypes.BUBBLE, h - d * 0.25D, j - e * 0.25D, k - g * 0.25D, d, e, g);
-				}
-
-				m = this.getDragInWater();
-			}
 
 			this.setVelocity(vec3d.multiply((double) m));
 			if (!this.hasNoGravity() && !bl) {
 				Vec3d vec3d5 = this.getVelocity();
 				this.setVelocity(vec3d5.x, vec3d5.y - 0.05000000074505806D, vec3d5.z);
 			}
-
 			this.updatePosition(h, j, k);
 			this.checkBlockCollision();
 		}
-	}
-
-	private void method_26352() {
-		this.inGround = false;
-		Vec3d vec3d = this.getVelocity();
-		this.setVelocity(vec3d.multiply((double) (this.random.nextFloat() * 0.2F),
-				(double) (this.random.nextFloat() * 0.2F), (double) (this.random.nextFloat() * 0.2F)));
-		this.ticksInAir = 0;
 	}
 
 	public void initFromStack(ItemStack stack) {
