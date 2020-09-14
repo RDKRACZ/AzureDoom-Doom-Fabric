2f956d831b5b6fb0b79ef8a5ee9b56f3b1c8a771
AzureZhen <rcraft@cyberconnects2.com>
2020-09-13 20:31:26 -0400

    Updated Human Models.

--- src/main/java/mod/azure/doom/client/render/PossessedSoldierRender.java
+++ src/main/java/mod/azure/doom/client/render/PossessedSoldierRender.java
@@ -1,7 +1,7 @@
 package mod.azure.doom.client.render;
 
 import mod.azure.doom.DoomMod;
-import mod.azure.doom.client.models.DemonModel;
+import mod.azure.doom.client.models.HumanModel;
 import mod.azure.doom.entity.PossessedSoldierEntity;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
@@ -9,12 +9,12 @@ import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
 import net.minecraft.util.Identifier;
 
 public class PossessedSoldierRender
-		extends MobEntityRenderer<PossessedSoldierEntity, DemonModel<PossessedSoldierEntity>> {
+		extends MobEntityRenderer<PossessedSoldierEntity, HumanModel<PossessedSoldierEntity>> {
 
 	protected static final Identifier TEXTURE = new Identifier(DoomMod.MODID, "textures/entity/possessedsoldier.png");
 
 	public PossessedSoldierRender(EntityRenderDispatcher renderManagerIn) {
-		super(renderManagerIn, new DemonModel<PossessedSoldierEntity>(0.5f, false), 0.5f);
+		super(renderManagerIn, new HumanModel<PossessedSoldierEntity>(0.5f, false), 0.5f);
 		this.addFeature(new HeldItemFeatureRenderer<>(this));
 
 	}
--- src/main/java/mod/azure/doom/client/render/UnwillingRender.java
+++ src/main/java/mod/azure/doom/client/render/UnwillingRender.java
@@ -1,19 +1,18 @@
 package mod.azure.doom.client.render;
 
 import mod.azure.doom.DoomMod;
-import mod.azure.doom.client.models.DemonModel;
+import mod.azure.doom.client.models.HumanModel;
 import mod.azure.doom.entity.UnwillingEntity;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
-public class UnwillingRender extends MobEntityRenderer<UnwillingEntity, DemonModel<UnwillingEntity>> {
+public class UnwillingRender extends MobEntityRenderer<UnwillingEntity, HumanModel<UnwillingEntity>> {
 
-	protected static final Identifier TEXTURE = new Identifier(DoomMod.MODID,
-			"textures/entity/unwilling.png");
+	protected static final Identifier TEXTURE = new Identifier(DoomMod.MODID, "textures/entity/unwilling.png");
 
 	public UnwillingRender(EntityRenderDispatcher renderManagerIn) {
-		super(renderManagerIn, new DemonModel<UnwillingEntity>(0.5f, false), 0.5f);
+		super(renderManagerIn, new HumanModel<UnwillingEntity>(0.5f, false), 0.5f);
 	}
 
 	@Override
