d31682aae3e1a8ae7215787734ae860dc0cf9330
AzureZhen <rcraft@cyberconnects2.com>
2020-10-08 18:40:58 -0400

    Fixed Pinky and animation

--- src/main/java/mod/azure/doom/entity/PinkyEntity.java
+++ src/main/java/mod/azure/doom/entity/PinkyEntity.java
@@ -76,7 +76,7 @@ public class PinkyEntity extends DemonEntity implements IAnimatedEntity {
 	public static DefaultAttributeContainer.Builder createMobAttributes() {
 		return LivingEntity.createLivingAttributes().add(EntityAttributes.GENERIC_FOLLOW_RANGE, 25.0D)
 				.add(EntityAttributes.GENERIC_MAX_HEALTH, 75.0D).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 6.0D)
-				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.15D)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D)
 				.add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 1.0D);
 	}
 
