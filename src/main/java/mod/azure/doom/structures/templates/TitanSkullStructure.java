29620524dbc9eaede13b87886bada73fcc02dccb
AzureZhen <rcraft@cyberconnects2.com>
2021-06-11 23:30:51 -0400

    checks for the top block now so it doesn't spawn into the terrain anymore

--- src/main/java/mod/azure/doom/structures/templates/TitanSkullStructure.java
+++ src/main/java/mod/azure/doom/structures/templates/TitanSkullStructure.java
@@ -2,6 +2,7 @@ package mod.azure.doom.structures.templates;
 
 import mod.azure.doom.structures.generator.TitanSkullGenerator;
 import mod.azure.doom.util.registry.ModEntityTypes;
+import net.minecraft.block.BlockState;
 import net.minecraft.structure.MarginedStructureStart;
 import net.minecraft.structure.PoolStructurePiece;
 import net.minecraft.structure.StructureManager;
@@ -11,12 +12,14 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.SpawnSettings;
 import net.minecraft.world.biome.SpawnSettings.SpawnEntry;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.ChunkRandom;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.VerticalBlockSample;
 import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
 
@@ -43,8 +46,13 @@ public class TitanSkullStructure extends StructureFeature<StructurePoolFeatureCo
 	protected boolean shouldStartAt(ChunkGenerator chunkGenerator, BiomeSource biomeSource, long worldSeed,
 			ChunkRandom random, ChunkPos pos, Biome biome, ChunkPos chunkPos, StructurePoolFeatureConfig config,
 			HeightLimitView world) {
-		return random.nextInt(5) < 2;
-
+		BlockPos centerOfChunk = new BlockPos(pos.x, 0, pos.z);
+		int landHeight = chunkGenerator.getHeightInGround(centerOfChunk.getX(), centerOfChunk.getZ(),
+				Heightmap.Type.WORLD_SURFACE_WG, world);
+		VerticalBlockSample columnOfBlocks = chunkGenerator.getColumnSample(centerOfChunk.getX(), centerOfChunk.getZ(),
+				world);
+		BlockState topBlock = columnOfBlocks.getState(centerOfChunk.up(landHeight));
+		return topBlock.getFluidState().isEmpty();
 	}
 
 	public static class Start extends MarginedStructureStart<StructurePoolFeatureConfig> {
